84452fbe03155628e16500f9ecf4810d1f95e413
Akshadachavan02 <124502943+Akshadachavan02@users.noreply.github.com>
2024-12-30 13:18:55 +0530

    School managment system

---
+++ school_management/school/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.name
---
+++ school_management/school/serial.py
@@ -0,0 +1,18 @@
+# filepath: /c:/Users/Admin/Desktop/exonect/school_management/school/serializers.py
+from rest_framework import serializers
+from .models import Student, Teacher, Course
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
